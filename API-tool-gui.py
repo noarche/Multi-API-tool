8101bc0b56180f581415c603b77c92ec3b715fb4
noarche <n0arch@pm.me>
2024-07-15 20:01:54 -0400

    Update API-tool-gui.py

--- API-tool-gui.py
+++ API-tool-gui.py
@@ -2,7 +2,6 @@ import PySimpleGUI as sg
 import requests
 import json
 
-
 sg.theme('DarkBlack')
 sg.theme_background_color('black')
 sg.theme_text_color('Red')
@@ -52,11 +51,49 @@ def get_lyrics(artist, song):
     try:
         data = response.json()
         lyrics = data.get('lyrics', 'No lyrics found')
-
         lyrics = "\n".join([line for line in lyrics.split('\n') if line.strip()])
     except json.decoder.JSONDecodeError:
         lyrics = "Error fetching lyrics"
     display_result(lyrics)
+
+def query_oogabooga_api(user_message):
+    base_url = 'http://127.0.0.1:5000'
+    api = OogaBoogaAPI(base_url)
+    try:
+        response_data = api.get_chat_completion(user_message)
+        results = []
+        for message in response_data.get("choices", []):
+            results.append(message["message"]["content"])
+        result = "\n".join(results)
+    except Exception as e:
+        result = f'Error: {e}'
+    display_result(result)
+
+class OogaBoogaAPI:
+    def __init__(self, base_url):
+        self.base_url = base_url
+        self.headers = {
+            'Content-Type': 'application/json'
+        }
+
+    def get_chat_completion(self, user_message, mode="instruct", instruction_template="Alpaca"):
+        data = {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": user_message
+                }
+            ],
+            "mode": mode,
+            "instruction_template": instruction_template
+        }
+        url = f'{self.base_url}/v1/chat/completions'
+        response = requests.post(url, headers=self.headers, json=data, verify=False)
+        
+        if response.status_code == 200:
+            return response.json()
+        else:
+            response.raise_for_status()
 
 def display_result(result):
     window['-TEXTBOX-'].update(result)
@@ -66,13 +103,13 @@ layout = [
     [sg.Button('Reverse ZipCode Lookup', size=(19, 1), key='-REVERSE_ZIP-')],
     [sg.Button('Domain Name Check', size=(19, 1), key='-DOMAIN_CHECK-')],
     [sg.Button('Lyrics Lookup', size=(19, 1), key='-LYRICS_LOOKUP-')],
+    [sg.Button('Query OogaBooga API', size=(19, 1), key='-QUERY_OOGA-')],
     [sg.Button('Help', size=(19, 1), key='-HELP-')],
-    [sg.Multiline(size=(80, 30), key='-TEXTBOX-', autoscroll=True, disabled=True, enable_events=True)]
+    [sg.Multiline(size=(80, 30), key='-TEXTBOX-', autoscroll=True, disabled=True, enable_events=True)],
+    [sg.InputText('', size=(80, 1), key='-USER_INPUT-')]
 ]
 
-
 window = sg.Window('API Data Fetcher', layout, finalize=True, resizable=False, alpha_channel=0.9)
-
 
 while True:
     event, values = window.read()
@@ -93,6 +130,10 @@ while True:
         song = sg.popup_get_text('Enter Song Title')
         if artist and song:
             get_lyrics(artist, song)
+    elif event == '-QUERY_OOGA-':
+        user_message = values['-USER_INPUT-']
+        if user_message:
+            query_oogabooga_api(user_message)
     elif event == '-HELP-':
         help_info = """
 
